1f385ba8458071ee3d58a8e492ec33ecef366a5d
Simon <simonmircevski6@gmail.com>
2023-04-24 16:57:16 +0200

    first commit

---
+++ aspnetserver/Data/AppDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace aspnetserver.Data
+{
+    internal sealed class AppDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("Data Source=./Data/AppDB.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Post[] postToSeed = new Post[6];
+            for(int i = 0; i < 6; i++)
+            {
+                postToSeed[i] = new Post
+                {
+                    PostId = i+10,
+                    Title = $"Post {i}",
+                    Content = $"This is post {i} and it has some very interesting content. I have also liked the video and subscribed."
+                };
+
+            }
+            modelBuilder.Entity<Post>().HasData(postToSeed);
+        }
+    }
+}
---
+++ aspnetserver/Data/Migrations/20230424131737_FirstMigration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace aspnetserver.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 100000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[,]
+                {
+                    { 10, "This is post 0 and it has some very interesting content. I have also liked the video and subscribed.", "Post 0" },
+                    { 11, "This is post 1 and it has some very interesting content. I have also liked the video and subscribed.", "Post 1" },
+                    { 12, "This is post 2 and it has some very interesting content. I have also liked the video and subscribed.", "Post 2" },
+                    { 13, "This is post 3 and it has some very interesting content. I have also liked the video and subscribed.", "Post 3" },
+                    { 14, "This is post 4 and it has some very interesting content. I have also liked the video and subscribed.", "Post 4" },
+                    { 15, "This is post 5 and it has some very interesting content. I have also liked the video and subscribed.", "Post 5" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
